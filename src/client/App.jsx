9a1ea0eee5b1faf0e36caa53f18bd87143042040
Azuriru <kegumine@gmail.com>
2023-03-26 12:42:36 +0800

    pwetty or not pwetty

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { SOFTSERVE } from '../data.json';
+import './App.css';
 
 // function Date() {
 //     // this is not a component
@@ -23,7 +24,7 @@ function Group({ title, children }) {
     )
 }
 
-function Input({ type, label, value, onInput }) {
+function Input({ type, label, value, arrows, onInput }) {
     return (
         <div className="input">
             <div className="label">{label}</div>
@@ -32,6 +33,13 @@ function Input({ type, label, value, onInput }) {
                 value={value}
                 onInput={e => onInput(e.target.value)}
             />
+            {
+                arrows &&
+                <div className="arrows">
+                    <div className="up" onClick={() => onInput(val => val + 1)}></div>
+                    <div className="down" onClick={() => onInput(val => val - 1)}></div>
+                </div>
+            }
         </div>
     )
 }
@@ -63,99 +71,105 @@ export default function App() {
 
     return (
         <div id="App">
-            <Group title="Discounts">
-                <Input
-                    type="number"
-                    label="Soft Serve"
-                    value={ssd}
-                    onInput={setSoftServeDiscount}
-                />
-                <Input
-                    type="number"
-                    label="Food"
-                    value={fd}
-                    onInput={setFoodDiscount}
-                />
-                <Input
-                    type="number"
-                    label="Beverages"
-                    value={bd}
-                    onInput={setBeveragesDiscount}
-                />
-            </Group>
-            <Group title="Soft Serve">
-                <Input
-                    type="number"
-                    label="Dark Chocolate"
-                    value={ssdc}
-                    onInput={setChocoSSAmount}
-                />
-                <Input
-                    type="number"
-                    label="Caramel / Mixed"
-                    value={sscm}
-                    onInput={setCMSSAmount}
-                />
-            </Group>
-            <Group title="Month To Dates">
-                <Input
-                    type="number"
-                    label="Soft Serve MTD"
-                    value={ssMTD}
-                    onInput={setSSMTD}
-                />
-                <Input
-                    type="number"
-                    label="Food MTD"
-                    value={foodMTD}
-                    onInput={setFoodMTD}
-                />
-                <Input
-                    type="number"
-                    label="Beverages MTD"
-                    value={beveragesMTD}
-                    onInput={setBeveragesMTD}
-                />
-                <Input
-                    type="number"
-                    label="Retail MTD"
-                    value={retailMTD}
-                    onInput={setRetailMTD}
-                />
-            </Group>
-            <Group title="Miscellaneous">
-                <Input
-                    type="number"
-                    label="Cracked Cones"
-                    value={cc}
-                    onInput={setCC}
-                />
-                <Input
-                    type="number"
-                    label="Total"
-                    value={total}
-                    onInput={setTotal}
-                />
-                <Input
-                    type="number"
-                    label="Transactions"
-                    value={transactions}
-                    onInput={setTransactions}
-                />
-                <Input
-                    type="number"
-                    label="Quantity"
-                    value={quantity}
-                    onInput={setQuantity}
-                />
-                <Input
-                    type="number"
-                    label="CRM"
-                    value={crm}
-                    onInput={setCRM}
-                />
-            </Group>
-            
+            <div className="wrapper">
+                <div className="header">Sales Report Generator</div>
+                <div className="body">
+                    <Group title="Discounts">
+                        <Input
+                            type="number"
+                            label="Soft Serve"
+                            value={ssd}
+                            onInput={setSoftServeDiscount}
+                        />
+                        <Input
+                            type="number"
+                            label="Food"
+                            value={fd}
+                            onInput={setFoodDiscount}
+                        />
+                        <Input
+                            type="number"
+                            label="Beverages"
+                            value={bd}
+                            onInput={setBeveragesDiscount}
+                        />
+                    </Group>
+                    <Group title="Soft Serve">
+                        <Input
+                            type="number"
+                            label="Dark Chocolate"
+                            value={ssdc}
+                            arrows={true}
+                            onInput={setChocoSSAmount}
+                        />
+                        <Input
+                            type="number"
+                            label="Caramel / Mixed"
+                            value={sscm}
+                            arrows={true}
+                            onInput={setCMSSAmount}
+                        />
+                    </Group>
+                    <Group title="Month To Dates">
+                        <Input
+                            type="number"
+                            label="Soft Serve MTD"
+                            value={ssMTD}
+                            onInput={setSSMTD}
+                        />
+                        <Input
+                            type="number"
+                            label="Food MTD"
+                            value={foodMTD}
+                            onInput={setFoodMTD}
+                        />
+                        <Input
+                            type="number"
+                            label="Beverages MTD"
+                            value={beveragesMTD}
+                            onInput={setBeveragesMTD}
+                        />
+                        <Input
+                            type="number"
+                            label="Retail MTD"
+                            value={retailMTD}
+                            onInput={setRetailMTD}
+                        />
+                    </Group>
+                    <Group title="Miscellaneous">
+                        <Input
+                            type="number"
+                            label="Cracked Cones"
+                            value={cc}
+                            onInput={setCC}
+                        />
+                        <Input
+                            type="number"
+                            label="Total"
+                            value={total}
+                            onInput={setTotal}
+                        />
+                        <Input
+                            type="number"
+                            label="Transactions"
+                            value={transactions}
+                            onInput={setTransactions}
+                        />
+                        <Input
+                            type="number"
+                            label="Quantity"
+                            value={quantity}
+                            onInput={setQuantity}
+                        />
+                        <Input
+                            type="number"
+                            label="CRM"
+                            value={crm}
+                            onInput={setCRM}
+                        />
+                    </Group>
+                </div>
+            </div>
         </div>
     );
 }
