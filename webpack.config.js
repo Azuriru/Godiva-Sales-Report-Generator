e40c8a99a69a77a60fd3b646f1173b5ec885341e
Azuriru <kegumine@gmail.com>
2023-04-01 03:05:56 +0800

    ponp (linda o no)

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,20 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader']
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
             },
             {
                 test: /\.s[ac]ss$/i,
